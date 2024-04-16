62cc8b0f094fd859403808e14bf33f912dd4fb96
spencerka475 <153318409+spencerka475@users.noreply.github.com>
2024-04-16 14:01:02 -0400

    sdfghj

---
+++ Js querySelector project/qs.js
@@ -0,0 +1,23 @@
+// Kael Spencer
+// 16 APR 2024
+// javascript practice
+
+const student = document.querySelector('#student');
+const homeSchool = document.querySelector('#homeSchool');
+const city = document.querySelector('#city');
+
+document.body.style.backgroundColor = "lightgreen";
+
+student.style.color = 'var(--steelblue)'; 
+student.style.fontSize = '1.7rem';
+
+homeSchool.style.fontSize = '1.2rem';
+homeSchool.style.marginBottom = '30px';
+homeSchool.style.fontFamily = 'Helvetica, sans-serif';
+homeSchool.style.fontVariant = 'small-caps';
+homeSchool.style.fontStyle = 'italic';
+
+city.style.display = 'inline';
+city.style.color = 'salmon';
+city.style.padding = '10px';
+city.style.border = '4px dotted orange';
